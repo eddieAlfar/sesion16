809920740de8cbd89c9b9e5c0118e41506cd76c1
eddieAlfaro <edbie2415@hotmail.com>
2022-11-17 17:36:30 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+// //N1
+
+// let n1 = +prompt("Ingresa cant")
+// let n2 = +prompt("Ingresa cant")
+// //Operaciones 
+// let multipli= function(a,b){
+//     return a*b
+// }
+// let expon= function(a,b){
+//     return Math.pow(a,b)
+// }
+// let suma=(a,b)=>{return a+b}
+
+// let resta=(a,b)=>{return a-b}
+
+// var result=multipli(n1,n2)
+// alert(result)
+// result = expon(n1,n2)
+// alert(result)
+
+// result = resta(n1,n2);
+// alert(result)
+
+// //N2
+// let temp=+prompt("ingresar temperatura")
+//         let pasaraFarenheit = (grado)=> {
+//         return Farenheit = grado * 1,8 + 32;
+//         };
+//         var result = pasaraFarenheit(temp);
+//         alert(result)
+
+//         let pasaraCelsius = (grado)=> {
+//             return celsius = grado -32 * 1,8;
+//             };
+//             var result = pasaraCelsius(temp);
+//             alert(result)
+    
+
+
+//N3
+let n1 = +prompt("Ingresa cant")
+let n2 = +prompt("Ingresa cant")
+
+let suma=(a,b)=>{return a+b}
+
+result = suma(n1,n2);
+alert(`El resultado de la suma es: ${n1} y ${n2} es: ${result}`)
